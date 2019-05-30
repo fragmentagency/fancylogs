ab1148240456d90ef9d1688a5ca1d9030ef43dbf
Fragment Agency <hello@fragment.agency>
2019-05-30 13:27:52 +0200

    two exports

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,32 @@
 module.exports = {
   "env": {
-      "browser": true,
-      "es6": true
+    "browser": true,
+    "es6": true
   },
   "extends": "airbnb-base",
   "globals": {
-      "Atomics": "readonly",
-      "SharedArrayBuffer": "readonly"
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
   },
   "parserOptions": {
-      "ecmaVersion": 2018,
-      "sourceType": "module"
+    "ecmaVersion": 2018,
+    "sourceType": "module"
   },
   "rules": {
-      "semi": [2, "never"],
-      "max-len": ["error", {
-          "ignoreComments": true,
-          "ignoreStrings": true,
-          "ignoreUrls": true,
-          "ignoreTemplateLiterals": true,
-          code: 140,
-          tabWidth: 2
-      }],
-      "no-underscore-dangle": ["error", {
-          "allowAfterThis": true
-      }],
-      "no-plusplus": "off",
-      "prefer-rest-params": "off",
-      "no-console": "off",
+    "semi": [2, "never"],
+    "max-len": ["error", {
+      "ignoreComments": true,
+      "ignoreStrings": true,
+      "ignoreUrls": true,
+      "ignoreTemplateLiterals": true,
+      code: 140,
+      tabWidth: 2
+    }],
+    "no-underscore-dangle": ["error", {
+      "allowAfterThis": true
+    }],
+    "no-plusplus": "off",
+    "prefer-rest-params": "off",
+    "no-console": "off",
   },
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,59 @@
 const path = require('path')
-const WebpackMd5Hash = require('webpack-md5-hash')
-// const CleanWebpackPlugin = require('clean-webpack-plugin')
+// const WebpackMd5Hash = require('webpack-md5-hash')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
-module.exports = {
-  entry: {
-    main: './app/index.js',
-  },
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'dist'),
-    libraryTarget: 'commonjs2',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
+module.exports = [
+  {
+    entry: {
+      main: './app/index.js',
+    },
+    output: {
+      filename: 'index.js',
+      path: path.resolve(__dirname, 'dist'),
+      libraryTarget: 'commonjs2',
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
     ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+      ],
+    },
+    devServer: {
+      publicPath: '/',
+      contentBase: path.resolve(__dirname, './public'),
+      watchContentBase: true,
+      compress: true,
+      port: 9002,
+      // host: '192.168.1.32',
+    },
+  }, {
+    entry: {
+      main: './app/FancyLogs.js',
+    },
+    output: {
+      filename: 'FancyLogs.js',
+      path: path.resolve(__dirname, 'dist'),
+      libraryTarget: 'var',
+      library: 'FancyLogs',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+      ],
+    },
   },
-  plugins: [
-    // new CleanWebpackPlugin(),
-    new WebpackMd5Hash(),
-  ],
-  devServer: {
-    publicPath: '/',
-    contentBase: path.resolve(__dirname, './public'),
-    watchContentBase: true,
-    compress: true,
-    port: 9002,
-    // host: '192.168.1.32',
-  },
-}
+]
